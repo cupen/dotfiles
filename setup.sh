aaa49170000567c77560a314e8a4bc7aeef34069
cupen <xcupen@gmail.com>
2023-09-10 14:17:28 +0800

    fix git config

--- setup.sh
+++ setup.sh
@@ -164,7 +164,7 @@ setup_git(){
     git config --global user.name       cupen
     git config --global user.email      xcupen@gmail.com
     git config --global push.default    current
-    git config --global core.autocrlf   true
+    git config --global core.autocrlf   input
 }
 
 setup_fonts(){
